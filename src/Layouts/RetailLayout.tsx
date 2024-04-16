c0a03f587647fe8f68e4d94189e6c177689bc62c
Adrian R. <adrian400n@gmail.com>
2024-04-16 15:45:01 -0600

    feat: better ui

--- src/Layouts/RetailLayout.tsx
+++ src/Layouts/RetailLayout.tsx
@@ -1,14 +1,37 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
 
 export default function RetailLayout() {
+  const location = useLocation();
+
   return (
     <div className="flex flex-col h-screen bg-slate-100">
       <header className="flex justify-between gap-4 shadow-sm border p-4 bg-white">
         <h1 className="font-bold">Retail</h1>
         <div className="flex gap-4">
-          <Link to="products">Products</Link>
-          <Link to="cart">Cart</Link>
-          <Link to="orders">Orders</Link>
+          <NavLink
+            to="products"
+            className={({ isActive }) =>
+              isActive ? 'text-blue-500 font-bold' : 'text-gray-500'
+            }
+          >
+            Products
+          </NavLink>
+          <NavLink
+            to="cart"
+            className={({ isActive }) =>
+              isActive ? 'text-blue-500' : 'text-gray-500'
+            }
+          >
+            Cart
+          </NavLink>
+          <NavLink
+            to="orders"
+            className={({ isActive }) =>
+              isActive ? 'text-blue-500' : 'text-gray-500'
+            }
+          >
+            Orders
+          </NavLink>
         </div>
       </header>
       <div className=" bg-slate-100 p-4">
