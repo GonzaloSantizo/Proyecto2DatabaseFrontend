8cb731967713703d1c162c06b30f47dd3f0a8d40
GonzaloSantizo <san21504@uvg.edu.gt>
2024-04-16 18:08:53 -0600

    show suppliers

--- src/Pages/Manufacturer/Manufacturer.tsx
+++ src/Pages/Manufacturer/Manufacturer.tsx
@@ -1,29 +1,70 @@
-import React from 'react';
-import styles from './Manufacturer.module.css';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 
+type Supplier = {
+  id: string;
+  name: string;
+};
 
 type Product = {
-  id: number;
+  id: string;
   name: string;
-  price: number;
-  image_url: string;
 };
 
 function Manufacturer() {
-  return (
-    <div className={styles.manufacturerContainer}>
-      <div className="flex flex-col bg-white border border-gray-200 shadow-sm rounded-xl p-2 md:p-3 text-sm dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
-        Add information about the manufacturer
-      </div>
-      <div className={styles.manufacturerBody}>
+  const [suppliers, setSuppliers] = useState<Supplier[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
+  useEffect(() => {
+    const manufacturerId = 'id_yuya'; // Replace with actual manufacturer ID
+    axios.get(`http://localhost:4000/manufacturer/${manufacturerId}/supplier`)
+      .then(response => {
+        
+        setSuppliers(response.data);
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+
+  useEffect(() => {
+    const manufacturerId = 'id_yuya'; 
+    axios.get(`http://localhost:4000/manufacturer/${manufacturerId}/products`)
+      .then(response => {
+        setProducts(response.data);
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <h1>Suppliers for Makeup Manufacturer:</h1>
+        {Array.isArray(suppliers) && suppliers.map((supplier, index) => (
+          <div key={index}>
+            {supplier.name}
+          </div>
+        ))}
       </div>
+
+      <div>
+        <h1>Products for Manufacturer:</h1>
+        {Array.isArray(products) && products.map((product, index) => (
+          <div key={index}>
+            {product.name}
+          </div>
+        ))}
+      </div>
+
     </div>
+
 
   );
 }
 
-export default  Manufacturer
+export default Manufacturer;
